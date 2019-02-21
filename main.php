5aa59ee016a8116a35e2cc2d39b2216650ee2613
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-02-21 14:52:06 -0500

    Built newline

---
+++ main.php
@@ -0,0 +1,18 @@
+<?php
+
+$dealerHandValue = 0;
+$playerHandValue = 0;
+
+while ($dealerHandValue < 17) {
+    //echo "Bet or stay?";
+    $line = readline("Bet or stay \n");
+
+    if ($line === "bet"){
+        echo "game over. \n";
+        exit();
+    }
+
+
+
+
+}
