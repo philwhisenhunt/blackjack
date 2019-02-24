5e875d8742afc9feeba58641bcfb81bad6a6888c
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-02-23 22:21:19 -0500

    array to string conversion needs solving

--- calculateHandValue.php
+++ calculateHandValue.php
@@ -4,10 +4,12 @@ require 'cardValueMaker.php';
 
 function calculateHandValue($playerHand){
 
+    $cardValueArray= [];
     for($i=0; $i<count($playerHand); $i++){
         $splitHand = str_split($playerHand[$i]);
 
     $cardValueArray[] = cardValueMaker($splitHand);
+    echo "The cardValueArray is $cardValueArray";
 
     //echo "The cardValueArray is ";
     //print_r($cardValueArray);
