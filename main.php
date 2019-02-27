2679f8713ee6730ef8e2c07a4e29e9751b86a72b
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-02-26 21:33:17 -0500

    built card deck

--- main.php
+++ main.php
@@ -9,7 +9,10 @@ $dealersCards = [];
 //$cards = ['AH','JC', 'KH', '9C', 'JH' ];
 //blank cards $cards = ['A','K','Q','J','T', '9', '8', '7', '6', '5', '4', '3', '2'];
 
-$cards = ['AH','KH','QH','JH','TH', '9H', '8H', '7H', '6H', '5H', '4H', '3H', '2H'];
+$cards = ['AH','KH','QH','JH','TH', '9H', '8H', '7H', '6H', '5H', '4H', '3H', '2H',
+ 'AS','KS','QS','JS','TS', '9S', '8S', '7S', '6S', '5S', '4S', '3S', '2S',
+ 'AD','KD','QD','JD','TD', '9D', '8D', '7D', '6D', '5D', '4D', '3D', '2D', 
+ 'AC','KC','QC','JC','TC', '9C', '8C', '7C', '6C', '5C', '4C', '3C', '2C'];
 
 
 $playersCards[] = array_pop($cards);
