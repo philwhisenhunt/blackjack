92bcdba44facea72a3437f19be6db0ee0e22fb69
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-02-27 08:09:14 -0500

    Shuffling cards now

--- main.php
+++ main.php
@@ -16,6 +16,9 @@ $cards = ['AH','KH','QH','JH','TH', '9H', '8H', '7H', '6H', '5H', '4H', '3H', '2
  'AD','KD','QD','JD','TD', '9D', '8D', '7D', '6D', '5D', '4D', '3D', '2D', 
  'AC','KC','QC','JC','TC', '9C', '8C', '7C', '6C', '5C', '4C', '3C', '2C'];
 
+shuffle($cards);
+print_r($cards);
+die();
 
 $playersCards[] = array_pop($cards);
 $playersCards[] = array_pop($cards);
