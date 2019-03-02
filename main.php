555f505562324da7314a67358d8a367cc98fd016
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-03-02 17:15:02 -0500

    Minor change, but checking in before bigger rearrange

--- main.php
+++ main.php
@@ -19,8 +19,9 @@ $cards = ['AH','KH','QH','JH','TH', '9H', '8H', '7H', '6H', '5H', '4H', '3H', '2
  'AD','KD','QD','JD','TD', '9D', '8D', '7D', '6D', '5D', '4D', '3D', '2D', 
  'AC','KC','QC','JC','TC', '9C', '8C', '7C', '6C', '5C', '4C', '3C', '2C'];
 
-shuffle($cards);
-echo "Shuffling cards ... \n";
+ echo "Shuffling cards ... \n";
+ shuffle($cards);
+
 // print_r($cards);
 // die();
 
@@ -28,6 +29,8 @@ $playersCards[] = array_pop($cards);
 $playersCards[] = array_pop($cards);
 $dealersCards[] = array_pop($cards);
 $dealersCards[] = array_pop($cards);
+
+echo "Your cards are:\n";
 showHand($playersCards);
 
 //while ($dealerHandValue < 17 && $playerHandValue <22) {
