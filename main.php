a8b0fe91ad0b5b62d31d8bc3a49469addfa1d6c2
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-03-15 15:08:58 -0400

    Checking in progress, laid ground work for converting aceCheck to function

--- main.php
+++ main.php
@@ -131,10 +131,16 @@ while ($wantToPlay) {
                 for($i=0; $i<count($cardValueArray); $i++){
                     // echo 'The variable $cardValueArray[$i] is ' . $cardValueArray[$i] . "\n";
                     if($cardValueArray[$i] == 11){
+                        //pop the card off the array, so that we can check for more aces.
+                        //probably should move this to a function.
                         $playerHandValue -= 10;
                         echo 'The $playerHandValue is ' . $playerHandValue . "\n";
 
                         if($playerHandValue >21){
+
+
+
+
                             $playerHandStatus = false;
                             echo "You busted, you lose. \n";
                             $bankAccount -= $betAmount;
