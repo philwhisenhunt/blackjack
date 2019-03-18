cad532ce8b7a5fd3c2c31a4aa346fe6b5e953bd5
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-03-18 09:12:15 -0400

    Thinking through aceCheck

--- aceCheck.php
+++ aceCheck.php
@@ -6,6 +6,12 @@ function aceCheck($cardValueArray){
         if($cardValueArray[$i] == 11){
             //pop the card off the array, so that we can check for more aces.
             //probably should move this to a function.
+
+            //should I pop that card off?
+
+            //even better, count the number of things with a value of 11
+            //ace count
+            //loop through, and if the total is still above 21, then substract 10 each time. 
             $playerHandValue -= 10;
             echo 'The $playerHandValue is ' . $playerHandValue . "\n";
         }
