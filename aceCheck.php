ced7c51e5a190da320eb3321ed256f7af8d0bff0
Philip Whisenhunt <philip.whisenhunt@gmail.com>
2019-03-20 09:16:09 -0400

    Checking in clean up before making calculation change

--- aceCheck.php
+++ aceCheck.php
@@ -5,13 +5,12 @@ function aceCheck($cardValueArray){
         while(in_array(11, $cardValueArray) && array_sum($cardValueArray) > 21){
             //echo "The while statement is still true";
             for($i=0; $i<count($cardValueArray); $i++){
-                // echo 'The variable $cardValueArray[$i] is ' . $cardValueArray[$i] . "\n";
-                // echo '$i is at ' . $i . "\n";
+                
+                //Change the value of an ace to 1
                 if($cardValueArray[$i] == 11){
                     $cardValueArray[$i] = 1;
-                    //echo '$cardValueArray[$i] is ' . $cardValueArray[$i] . "\n";
-                    //echo '$cardValueArray value is ' . array_sum($cardValueArray) . "\n";
-    
+                    
+                    //then check if that was enough to reduce it below 22
                     if(array_sum($cardValueArray) < 21 ){
                         //echo "Success! \n"; 
                         return array_sum($cardValueArray);
